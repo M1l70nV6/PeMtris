8a523cc48f67945f76e3853fcc65ee2b6c8584cf
M1l70NV6 <miltonvg@uets.edu.ec>
2023-12-24 01:49:11 -0500

    V1.0.0
    It Works

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -10,24 +10,27 @@ import { Component, OnInit } from '@angular/core';
     2.5 Sticky
   */
 export class HomePage implements OnInit {
-  private WIDTH_BOARD: number = 10;
-  private HEIGHT_BOARD: number = 20;
+  private readonly WIDTH_BOARD: number = 10;
+  private readonly HEIGHT_BOARD: number = 20;
   private SHAPES = [
     [[1, 1, 1, 1]],
     [
       [1, 1],
       [1, 1],
     ],
+    [
+      [1, 1, 0],
+      [0, 1, 1],
+    ],
   ];
   private board: number[] = [];
   private tetramino: any;
-  private dx:number=0;
-  private dy:number=0;
+
   constructor() {}
   ngOnInit(): void {
     this.initBoard();
     this.generateTetramino();
-    this.drawTetramino(this.dx,this.dy);
+    this.drawTetramino();
   }
 
   //1. Tablero
@@ -48,55 +51,97 @@ export class HomePage implements OnInit {
     const shape = this.SHAPES[Math.floor(Math.random() * this.SHAPES.length)];
     this.tetramino = {
       y: 0,
-      x: Math.floor(this.WIDTH_BOARD / 2 - shape.length / 2),
+      x: Math.floor(this.WIDTH_BOARD / 2 - shape[0].length / 2),
       shape: shape,
-      newX:0,//2.3 Colisión
-      newY:0//2.3 Colisión
     };
   }
-  eraseTetramino(dx:number,dy:number){
+  eraseTetramino() {
     for (let y = 0; y < this.tetramino.shape.length; y++) {
       for (let x = 0; x < this.tetramino.shape[y].length; x++) {
-        const newX= this.tetramino.x + x + dx; 
-        const newY= this.tetramino.y + y +dy;
-        this.board[newY*this.WIDTH_BOARD+newX] = 0; 
+        const newX = this.tetramino.x + x;
+        const newY = this.tetramino.y + y;
+        this.board[newY * this.WIDTH_BOARD + newX] = 0;
       }
     }
   }
-  drawTetramino(dx:number,dy:number){
+  drawTetramino() {
     for (let y = 0; y < this.tetramino.shape.length; y++) {
       for (let x = 0; x < this.tetramino.shape[y].length; x++) {
-        const newX= this.tetramino.x + x +dx; 
-        const newY= this.tetramino.y + y +dy;
-        this.board[newY*this.WIDTH_BOARD+newX] = this.tetramino.shape[y][x]; 
-        this.tetramino.newX=newX;//2.3 Colisión
-        this.tetramino.newY=newY;//2.3 Colisión
+        const newX = this.tetramino.x + x;
+        const newY = this.tetramino.y + y;
+        this.board[newY * this.WIDTH_BOARD + newX] = this.tetramino.shape[y][x];
+      }
+    }
+  }
+  stickTetramino() {
+    for (let y = 0; y < this.tetramino.shape.length; y++) {
+      for (let x = 0; x < this.tetramino.shape[y].length; x++) {
+        const newX = this.tetramino.x + x;
+        const newY = this.tetramino.y + y;
+        if (this.tetramino.shape[y][x] === 1) {
+          this.board[newY * this.WIDTH_BOARD + newX] = 2;
+        } else {
+          this.board[newY * this.WIDTH_BOARD + newX] = 0;
+        }
+      }
+    }
+  }
+  checkTetramino(dx: number, dy: number): boolean {
+    for (let y = 0; y < this.tetramino.shape.length; y++) {
+      for (let x = 0; x < this.tetramino.shape[y].length; x++) {
+        const newX = this.tetramino.x + x + dx;
+        const newY = this.tetramino.y + y + dy;
+          if (newX < 0 || newX >= this.WIDTH_BOARD) return false;
+          if (newY >= this.HEIGHT_BOARD) return false;
+          if (this.board[newY * this.WIDTH_BOARD + newX] === 2) return false;
+      }
+    }
+    return true;
+  }
+
+  clearLines() {
+    for (let y = 0; y < this.HEIGHT_BOARD; y++) {
+      let count = 0; // Move count initialization inside the loop for each row
+      for (let x = 0; x < this.WIDTH_BOARD; x++) {
+        if (this.board[y * this.WIDTH_BOARD + x] === 2) {
+          count++;
+        }
+      }
+      if (count === this.WIDTH_BOARD) {
+        this.board.splice(y * this.WIDTH_BOARD, this.WIDTH_BOARD);
+        // Add a new empty line at the top
+        this.board.unshift(...Array(this.WIDTH_BOARD).fill(0));
       }
     }
   }
 
-
+  moveTetramino(dx: number = 0, dy: number = 0) {
+    const valid = this.checkTetramino(dx, dy);
+    if (!valid && dy) {
+      this.stickTetramino();
+      this.clearLines();
+      this.generateTetramino();
+    }
+    if (!valid) {
+      return;
+    }
+    this.eraseTetramino();
+    this.tetramino.x += dx;
+    this.tetramino.y += dy;
+    this.drawTetramino();
+  }
   //2.2 Movimiento
-  onKeyDown(e:KeyboardEvent):void{
-
-    this.eraseTetramino(this.dx,this.dy);//borrar la antigua figura
-    switch(e.key){
+  onKeyDown(e: KeyboardEvent): void {
+    switch (e.key) {
       case 'ArrowDown':
-        if(this.tetramino.newY < this.HEIGHT_BOARD-1){//2.3 Colisión
-          this.dy++;
-        }
-      break;
+        this.moveTetramino(0, 1);
+        break;
       case 'ArrowLeft':
-        if(this.tetramino.newX >= this.tetramino.shape[0].length){//2.3 Colisión
-          this.dx--;
-        }
-      break;
+        this.moveTetramino(-1, 0);
+        break;
       case 'ArrowRight':
-        if(this.tetramino.newX <= this.WIDTH_BOARD-2){//2.3 Colisión
-          this.dx++;
-        }
-      break;
+        this.moveTetramino(1, 0);
+        break;
     }
-    this.drawTetramino(this.dx,this.dy);//dibujar la nueva figura
   }
 }
